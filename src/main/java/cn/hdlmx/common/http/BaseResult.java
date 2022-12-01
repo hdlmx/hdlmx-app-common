9b24e8af9c99148c6d2ef60c9a72edd3e455889d
hdlmx <hdlmx@outlook.com>
2022-12-01 01:52:23 +0000

    重构BaseResult

--- src/main/java/cn/hdlmx/common/http/BaseResult.java
+++ src/main/java/cn/hdlmx/common/http/BaseResult.java
@@ -6,10 +6,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * @author hdlmx
+ */
 @ApiModel("基础Response")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class BaseResult<T> {
     @ApiModelProperty("消息")
     private String msg = "success";
@@ -21,5 +21,37 @@ public class BaseResult<T> {
     public BaseResult(Integer code, String msg) {
         this.msg = msg;
         this.code = code;
+    }
+
+    public BaseResult(Integer code, String msg, T data) {
+        this.msg = msg;
+        this.code = code;
+        this.data = data;
+    }
+
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public BaseResult<T> setData(T data) {
+        this.data = data;
+        return this;
     }
 }
--- src/main/java/cn/hdlmx/common/http/BaseResultUtils.java
+++ src/main/java/cn/hdlmx/common/http/BaseResultUtils.java
@@ -4,24 +4,57 @@ import cn.hdlmx.common.baseEnum.ResponseStatusEnum;
 
 public class BaseResultUtils<T> {
     /**
-     * 成功的请求
+     * @param data response body
+     *             成功的请求
      */
-    public BaseResult<T> success = new BaseResult<>(ResponseStatusEnum.SUCCESS.getCode(), ResponseStatusEnum.SUCCESS.getMsg());
+    public static <T> BaseResult<T> success(T data) {
+        return new BaseResult<>(ResponseStatusEnum.SUCCESS.getCode(), ResponseStatusEnum.SUCCESS.getMsg());
+    }
+
     /**
      * 失败的请求
+     *
+     * @param data response body
+     * @return response result
      */
-    public BaseResult<T> failed = new BaseResult<>(ResponseStatusEnum.ERROR.getCode(), ResponseStatusEnum.ERROR.getMsg());
+    public static <T> BaseResult<T> failed(T data) {
+        return new BaseResult<>(ResponseStatusEnum.ERROR.getCode(), ResponseStatusEnum.ERROR.getMsg());
+    }
+
+
     /**
      * 接口不存在
+     *
+     * @param data response body
+     * @return response result
      */
-    public BaseResult<T> notFound = new BaseResult<>(ResponseStatusEnum.NOT_FOUND.getCode(), ResponseStatusEnum.NOT_FOUND.getMsg());
+    public static <T> BaseResult<T> notFound(T data) {
+        return new BaseResult<>(ResponseStatusEnum.NOT_FOUND.getCode(), ResponseStatusEnum.NOT_FOUND.getMsg(), data);
+    }
+
     /**
      * 接口未认证
+     *
+     * @param data response body
+     * @return response result
      */
-    public BaseResult<T> unauthorized = new BaseResult<>(ResponseStatusEnum.UNAUTHORIZE.getCode(), ResponseStatusEnum.UNAUTHORIZE.getMsg());
+    public static <T> BaseResult<T> unauthorized(T data) {
+        return new BaseResult<>(ResponseStatusEnum.UNAUTHORIZE.getCode(), ResponseStatusEnum.UNAUTHORIZE.getMsg(), data);
+    }
 
+    /**
+     * 自定义请求类型
+     *
+     * @param code
+     * @param msg
+     * @return response result
+     */
     public BaseResult<T> buildResult(Integer code, String msg) {
         return new BaseResult<>(code, msg);
+    }
+
+    public static <T> BaseResult<T> buildResult(Integer code, String msg, T data) {
+        return new BaseResult<>(code, msg, data);
     }
 
 }
