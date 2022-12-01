b4fbe4e91bd04d522d53179b64076723b8f3c370
hdlmx <hdlmx@outlook.com>
2022-12-01 22:10:25 +0800

    分页类注解补充

--- src/main/java/cn/hdlmx/common/page/Page.java
+++ src/main/java/cn/hdlmx/common/page/Page.java
@@ -1,6 +1,7 @@
 package cn.hdlmx.common.page;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import jakarta.validation.constraints.Min;
 import lombok.Data;
 
@@ -9,15 +10,24 @@ import java.util.Objects;
 @ApiModel("分页")
 @Data
 public class Page {
+    @ApiModelProperty("当前页")
     @Min(1)
     private Integer page = 1;
+    @ApiModelProperty("分页大小")
     @Min(1)
     private Integer pageSize = 10;
+    @ApiModelProperty("总数")
     @Min(0)
     private Integer total = 0;
+    @ApiModelProperty("总页数")
     @Min(0)
     private Integer totalPage = 0;
 
+    /**
+     * 设置总数并计算页数
+     *
+     * @param total 总数
+     */
     public void setTotal(Integer total) {
         if (Objects.nonNull(total)) {
             this.total = total;
